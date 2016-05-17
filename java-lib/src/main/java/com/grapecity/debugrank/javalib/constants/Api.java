f061a54c089718b676d4a11583298c9257138d7e
ripple182 <chrisripple@chriss-mbp.componentone.com>
2016-05-17 12:09:30 -0400

    remove

--- java-lib/src/main/java/com/grapecity/debugrank/javalib/constants/Api.java
+++ java-lib/src/main/java/com/grapecity/debugrank/javalib/constants/Api.java
@@ -8,7 +8,7 @@ public class Api
     public static final String GITHUB_API = "https://raw.githubusercontent.com";
     public static final String HACKERRANK_API = "http://api.hackerrank.com";
 
-    public static final String HACKERRANK_API_KEY = "hackerrank|912642-602|41df1bd9d20c89dbc9ed667af33c9174babeb265";
+    public static final String HACKERRANK_API_KEY = "";
 
     public static final String GITHUB_API_OWNER = "grapecity";
     public static final String GITHUB_API_REPO = "DebugRank";
