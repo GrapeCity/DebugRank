81c5fbecc82b3d88d48f75f34f1065a3f4a2413b
ripple182 <chrisripple@chriss-mbp.componentone.com>
2016-05-17 11:55:03 -0400

    #8 RecyclerView / Pull to refresh

--- app/src/main/java/com/grapecity/debugrank/ui/base/BaseRecyclerViewActivity.java
+++ app/src/main/java/com/grapecity/debugrank/ui/base/BaseRecyclerViewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.OnScrollListener;
 
 import butterknife.Bind;
 import com.grapecity.debugrank.R;
@@ -28,9 +29,26 @@ public abstract class BaseRecyclerViewActivity<T extends RecyclerView.Adapter> e
     @Override
     protected void attachView()
     {
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
         adapter = getRecyclerViewAdapter();
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnScrollListener(new OnScrollListener()
+        {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState)
+            {
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
+            {
+                swipeRefreshLayout.setEnabled(linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0);
+            }
+        });
     }
 
     @Override
