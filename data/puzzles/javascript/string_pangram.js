65abb83b3ad660aa7af84709d8e7c02828b7bcec
ripple182 <chrisripple@chriss-mbp.componentone.com>
2016-05-17 10:22:55 -0400

    add 2x more javascript puzzles

--- data/puzzles/javascript/string_pangram.js
+++ data/puzzles/javascript/string_pangram.js
@@ -11,43 +11,50 @@
 //
 
 //
-//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-//  Scroll down, this code is necessary for HackerRank cloud compiling.
-//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-//
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
-
-//
 // Determine if the given input string is a Pangram.
 //
 function isStringPangram(input)
 {
-    return false; 
+    var lowercaseInput = input.trim().toLowerCase();
+
+    var arr = new Array(26);
+
+    for (var i = 0; i < 26; i++)
+    {
+        arr[i] = 0;
+    }
+
+    for (var i = 0; i < lowercaseInput.length; i++)
+    {
+        if (lowercaseInput[i] < 'b' || lowercaseInput[i] > 'y') 
+        { 
+            continue; 
+        }
+
+        var idx = lowercaseInput.charCodeAt(i) - 'a'.charCodeAt(0);
+
+        arr[idx]++;
+    }
+
+    for (var i = 0; i < 26; i++)
+    {
+        if (arr[i] == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 //
-// Don't edit any code inside this method.
+// Don't edit any code below this line.
 // Necessary for HackerRank cloud compiling.
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 // 
 function main()
 {
-    var input = readLine();
-    
-    var res = isStringPangram(input);
+    var res = isStringPangram(input_stdin);
 
     if(res)
     {
@@ -58,3 +65,17 @@ function main()
         console.log("not pangram");
     }
 }
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+
+process.stdin.on('data', function (data)
+{
+    input_stdin += data;
+});
+process.stdin.on('end', function ()
+{
+    main();    
+});
---
+++ data/puzzles/javascript/substring_compile.js
@@ -0,0 +1,69 @@
+//
+// The Substring class determines if a substring
+// exists between two strings.
+//
+// But unfortunately the class does not compile ):
+// 
+// Press the compile button in the bottom right corner
+// to get the compile errors in order to fix the code.
+//
+
+//
+    // Determine if a substring exists in two strings.
+    // 
+function doesSubstringExist(stringA, stringB)
+{
+    var map = {};
+
+    for (int i = 0; i < stringA.length; i++)
+    {
+        map[stringA[i]] = true;
+    }
+
+    for (var i = 0; i < stringB.length; i++)
+    {
+        if (map[stringB[i]])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//
+// Don't edit any code below this line.
+// Necessary for HackerRank cloud compiling.
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// 
+function main()
+{
+    var strings = input_stdin.split(',');
+    
+    var stringA = strings[0];
+    var stringB = strings1];
+
+    var res = doesSubstringExist(stringA, stringB);
+
+    if(res)
+    {
+        console.log("substring exists");
+    }
+    else
+    {
+        console.log("no substring exists");
+    }
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+
+process.stdin.on('data', function (data)
+{
+    input_stdin += data;
+});
+process.stdin.on('end', function ()
+{
+    main();    
+});
